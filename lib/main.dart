e10bc3516e72dbc46b6a6bbd5a777eaea2f5ac3b
Kenichi Ukai <[ken151102@gmail.com]>
2019-03-30 17:07:47 +0900

    add capture

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
+import 'package:screenshot/screenshot.dart';
 
 void main() {
   SystemChrome.setPreferredOrientations(
@@ -30,9 +33,10 @@ class Fletter extends StatefulWidget {
 class _FletterState extends State<Fletter> {
   List<LinePoints> lines = <LinePoints>[];
   List<Offset> nowPoints = <Offset>[];
+  ScreenshotController screenshotController = ScreenshotController();
+  File _imageFile;
   Color nowColor = Colors.blueGrey;
 
-  //detect touches
   void moveGestureDetector(DragUpdateDetails detail) {
     Offset p = Offset(detail.globalPosition.dx, detail.globalPosition.dy);
     setState(() {
@@ -59,43 +63,70 @@ class _FletterState extends State<Fletter> {
     });
   }
 
+  void _takeScreenShot() {
+    screenshotController.capture().then((File image) {
+      setState(() {
+        _imageFile = image;
+      });
+      print('DEBUG: image:$image');
+    }).catchError((onError) {
+      print('DEBUG: error:$onError');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Stack(children: [
-        new Image.asset("assets/comp.png"),
-        Container(
-          color: Color.fromRGBO(0, 0, 0, 0.05),
-          // decoration: BoxDecoration(color: Colors.white),
-          child: new Flex(
-            direction: Axis.vertical,
-            children: <Widget>[
-              Expanded(
-                child: AspectRatio(
-                  aspectRatio: 2.0,
-                  child: GestureDetector(
-                    child: CustomPaint(
-                      painter: PaintCanvas(lines, nowPoints, nowColor),
+    return new Screenshot(
+      controller: screenshotController,
+      child: Scaffold(
+        body: new Stack(children: [
+          new Image.asset("assets/comp.png"),
+          Container(
+            color: Color.fromRGBO(0, 0, 0, 0.05),
+            child: new Flex(
+              direction: Axis.vertical,
+              children: <Widget>[
+                Expanded(
+                  child: AspectRatio(
+                    aspectRatio: 2.0,
+                    child: GestureDetector(
+                      child: CustomPaint(
+                        painter: PaintCanvas(lines, nowPoints, nowColor),
+                      ),
+                      onHorizontalDragUpdate: moveGestureDetector,
+                      onVerticalDragUpdate: moveGestureDetector,
+                      onHorizontalDragStart: newGestureDetector,
+                      onVerticalDragStart: newGestureDetector,
                     ),
-                    onHorizontalDragUpdate: moveGestureDetector,
-                    onVerticalDragUpdate: moveGestureDetector,
-                    onHorizontalDragStart: newGestureDetector,
-                    onVerticalDragStart: newGestureDetector,
                   ),
-                ),
-              )
-            ],
+                )
+              ],
+            ),
           ),
+        ], fit: StackFit.expand),
+        //buttons
+        floatingActionButton: Column(
+          verticalDirection: VerticalDirection.up,
+          children: <Widget>[
+            FloatingActionButton(
+              onPressed: _tapClear,
+              backgroundColor: Colors.redAccent,
+              foregroundColor: Colors.white,
+              child: Icon(Icons.delete),
+            ),
+            Container(
+              margin: EdgeInsets.only(bottom: 16.0),
+              child: FloatingActionButton(
+                onPressed: _takeScreenShot,
+                backgroundColor: Colors.redAccent,
+                foregroundColor: Colors.white,
+                child: Icon(Icons.file_upload),
+              ),
+            ),
+          ],
         ),
-      ], fit: StackFit.expand),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _tapClear,
-        backgroundColor: Colors.redAccent,
-        foregroundColor: Colors.white,
-        child: Icon(Icons.delete),
       ),
     );
-    // );
   }
 }
 
@@ -115,8 +146,7 @@ class PaintCanvas extends CustomPainter {
     for (int i = 0; i < lines.length; i++) {
       LinePoints l = lines[i];
       for (int j = 1; j < l.points.length; j++) {
-        Offset p1 = l.points[j - 1];
-        Offset p2 = l.points[j];
+        Offset p1 = l.points[j - 1], p2 = l.points[j];
         p.color = l.lineColor;
         canvas.drawLine(p1, p2, p);
       }
